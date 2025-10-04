9ffbd9a668a2e867fcacc1a1a88b9c59bcfad63b
Yannis-Alouache <alouacehyannis1@gmail.com>
2025-10-04 22:02:49 +0200

    feat: update main

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './infrastructure/api/modules/app.module';
+import { configureSwagger } from './infrastructure/api/documentation/configure-swagger';
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+
+  configureSwagger(app);
+
   await app.listen(process.env.PORT ?? 3000);
-  console.log("App is running on port : " + (process.env.PORT ?? 3000));
+  console.log("🚀 Hexa Poll is running on port : " + (process.env.PORT ?? 3000));
 }
+
 bootstrap();
