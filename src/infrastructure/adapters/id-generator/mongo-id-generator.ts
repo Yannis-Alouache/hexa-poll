812d5877c87ac8e51aa096fb508b9297809ca2b0
Yannis-Alouache <alouacehyannis1@gmail.com>
2025-10-10 22:42:56 +0200

    feat: implement ObjectId generation in MongoIdGenerator

--- src/infrastructure/adapters/id-generator/mongo-id-generator.ts
+++ src/infrastructure/adapters/id-generator/mongo-id-generator.ts
@@ -1,7 +1,8 @@
+import mongoose from "mongoose";
 import {  IdGenerator } from "../../../domain/ports/out/id-generator";
 
 export class MongoIdGenerator implements IdGenerator {
-    generate(): string {
-        throw new Error("Method not implemented.");
+    generate(): string{
+        return new mongoose.Types.ObjectId().toHexString();
     }
 }
